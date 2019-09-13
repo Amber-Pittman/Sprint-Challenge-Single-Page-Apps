04d3a9ce9c27d0a3be968143da300456d9526beb
Amber-Pittman <amber@amberpittman.com>
2019-09-13 19:47:54 -0400

    Character info displays in Console

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
 
-import styled from "styled-components";
-
 
 export default function App() {
   return (
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default function CharacterCard() {
+export default function CharacterCard({character}) {
+  console.log("char card props", character);
   return <span>todo: character</span>;
 }
