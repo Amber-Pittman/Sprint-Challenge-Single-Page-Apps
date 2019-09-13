2617656e0f8619e3f82ad35ade5e40898d39e786
Amber-Pittman <amber@amberpittman.com>
2019-09-13 19:33:45 -0400

    Got CharacterList h2 displaying on page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
+
+import styled from "styled-components";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <CharacterList />
       {/* <TabNav /> */}
     </main>
   );
