42217438e69d5d9a6396a29166c7141a0cd86883
Amber-Pittman <amber@amberpittman.com>
2019-09-13 20:04:06 -0400

    Char Card visually displaying in browser

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,5 +2,21 @@ import React from "react";
 
 export default function CharacterCard({character}) {
   console.log("char card props", character);
-  return <span>todo: character</span>;
+  const { image, name, location, origin, } = character;
+
+  return (
+    <div>
+      <img 
+        src={`${image}`} 
+        alt={`This is ${name} from Adult Swim's Rick and Morty animated show`} 
+        data-pin-nopin="true" 
+      />
+      <h2>{name}</h2>
+      <p>Location: {location.name}</p>
+      <p>Origin: {origin.name}</p>
+      <br />
+
+      {/* Don't forget to provide Episode Info in each Char Card */}
+    </div>
+  )
 }
