e70e0545eeedfd0caca39bc7a70ac3a1c8ad8df9
Amber-Pittman <amber@amberpittman.com>
2019-09-13 20:22:02 -0400

    Unstyled Locations displaying in browser

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard({ location }) {
+  console.log(location);
+  const {name, type, dimension, residents} = location;
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p> {`${type} - ${dimension}`} </p>
+      <p> {`${residents.length} Residents`} </p> 
+    </div>
+  );
 }
