cc4dd1a75117ef3af79d90d7c3f1f64c0dc597c4
Amber-Pittman <amber@amberpittman.com>
2019-09-15 17:48:31 -0400

    Nav Tabs are displaying

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
+import { Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 import WelcomePage from "./WelcomePage";
 import CharacterList from "./CharacterList";
@@ -16,26 +16,24 @@ import EpisodeList from "./EpisodeList";
 
 export default function TabNav() {
 
-    const [tab, setTab] = useState("home");
-
     return(
         <Menu>
-            <Tab eventTab="home" title="Home Page">
-              <Icon name="home" /> 
-              <NavLink to="/" activeClassName="home"> Home Page </NavLink>
-            </Tab>
-            <Tab eventTab="character" title="Characters" >
-              <Icon name="users" />
-              <NavLink to="/character" activeClassName="characters">Characters</NavLink>
-            </Tab>
-            <Tab eventTab="location" title="Locations" >
-              <Icon name="map outline" />
-              <NavLink to="/location" activeClassName="locations">Locations</NavLink>
-            </Tab>
-            <Tab eventTab="episode" title="Episodes" >
-              <Icon name="video" />
-              <NavLink to="/episode" activeClassName="episodes">Episodes</NavLink>
-            </Tab>
+            <Menu.Item>
+                <Icon name="home" /> 
+                <NavLink to="/" activeClassName="home"> Home Page </NavLink>
+            </Menu.Item>
+            <Menu.Item>
+                <Icon name="users" />
+                <NavLink to="/character" activeClassName="characters">Characters</NavLink>
+            </Menu.Item>
+            <Menu.Item>
+                <Icon name="map outline" />
+                <NavLink to="/location" activeClassName="locations">Locations</NavLink>
+            </Menu.Item>
+            <Menu.Item>
+                <Icon name="video" />
+                <NavLink to="/episode" activeClassName="episodes">Episodes</NavLink>
+            </Menu.Item>
         </Menu>
     )
 
